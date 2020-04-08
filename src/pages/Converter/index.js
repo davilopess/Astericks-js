1080ced5ee6d364699b0627f1b5aea826c2dd03d
Davi Lopes <davi.lopes.amorim2014@hotmail.com>
2020-04-08 01:46:15 -0300

    frontend da aplicação

---
+++ src/pages/Converter/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+import { MdAutorenew } from "react-icons/md";
+import logoImg from '../../assets/Logo.png';
+
+export default function Converter() {
+    const [text, setText] = useState('');
+    const [textConverted, setConverted] = useState('');
+
+    function handleConverter(e) {
+        e.preventDefault();
+        setConverted(text.replace(/\w/g, '*'));
+
+    }
+
+    return (
+        <div className="converter-container">
+            <div className="container">
+                <section>
+                    <img src={logoImg} alt="Astericks" />
+                    {/* <h1>
+                        Digite um texto para ser convertido
+                    </h1> */}
+
+                </section>
+                <form onSubmit={handleConverter}>
+                    <textarea
+                        placeholder="Digite um texto para ser convertido"
+                        value={text}
+                        onChange={e => setText(e.target.value)}
+                    /><br />
+
+                    <button className="button" type="submit">
+                        <MdAutorenew size={22} color="fff" />
+                    </button>
+                    <span>
+                        {textConverted}
+
+                    </span>
+                </form>
+
+            </div>
+
+        </div>
+    );
+};
